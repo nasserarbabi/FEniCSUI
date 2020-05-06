e03322886a0cbb10d98d8a48c43dc957ff5e9f10
Nasser Arbabi <naser.arbaby@gmail.com>
2020-05-05 21:46:38 -0700

    authentication is removed

--- FEniCSUI/AnalysesHub/views.py
+++ FEniCSUI/AnalysesHub/views.py
@@ -1,16 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
+from dashboard.models import projects
 from .models import AnalysisConfig, SolverResults, SolverProgress
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import FormParser, JSONParser
 from rest_framework import status
 import docker
 import os
 from base64 import b64encode
+import json
 
 class solverConfig(APIView):
-    permission_classes = (IsAuthenticated,)
+
     parser_classes = [FormParser]
 
     def get(self, request, *args, **kwargs):
@@ -18,79 +19,67 @@ class solverConfig(APIView):
         return a list of entries within a given category
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            category = request.query_params.get('category')
-            paterntConfig = AnalysisConfig.objects.get(project=project)
-            if category in paterntConfig.config:
-                return Response(data=paterntConfig.config[category], status=status.HTTP_200_OK)
-            else:
-                return Response(data="The category {} does not exist".format(category), status=status.HTTP_204_NO_CONTENT)
+        category = request.query_params.get('category')
+        parentConfig = AnalysisConfig.objects.get(project=project)
+        if category in parentConfig.config:
+            return Response(data=parentConfig.config[category], status=status.HTTP_200_OK)
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            return Response(data="The category {} does not exist".format(category), status=status.HTTP_204_NO_CONTENT)
+
 
     def post(self, request, *args, **kwargs):
         """
         create a new category for solver configuration
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            data = request.data.dict()
-            category = request.query_params.get('category')
-            paterntConfig = AnalysisConfig.objects.get(project=project)
-            if category not in paterntConfig.config:
-                paterntConfig.config[category] = []
-            paterntConfig.config[category].append(data)
-            paterntConfig.save()
-            return Response(data=paterntConfig.config[category], status=status.HTTP_201_CREATED)
-        else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+        data = request.data.dict()
+        category = request.query_params.get('category')
+        parentConfig = AnalysisConfig.objects.get(project=project)
+        if category not in parentConfig.config:
+            parentConfig.config[category] = []
+        parentConfig.config[category].append(data)
+        parentConfig.save()
+        return Response(data=parentConfig.config[category], status=status.HTTP_201_CREATED)
 
     def put(self, request, *args, **kwargs):
         """
         Edit an existing category entry's data
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            data = request.data.dict()
-            category = request.query_params.get('category')
-            list_id = int(request.query_params.get('id'))
-            paterntConfig = AnalysisConfig.objects.get(project=project)
-            if category in paterntConfig.config:
-                if list_id >= 0 and list_id < len(paterntConfig.config[category]):
-                    paterntConfig.config[category][list_id] = data
-                    paterntConfig.save()
-                    return Response(data=paterntConfig.config[category], status=status.HTTP_200_OK)
-                else:
-                    return Response(data="No entry with the id={}".format(list_id), status=status.HTTP_204_NO_CONTENT)
+        data = request.data.dict()
+        category = request.query_params.get('category')
+        list_id = int(request.query_params.get('id'))
+        parentConfig = AnalysisConfig.objects.get(project=project)
+        if category in parentConfig.config:
+            if list_id >= 0 and list_id < len(parentConfig.config[category]):
+                parentConfig.config[category][list_id] = data
+                parentConfig.save()
+                return Response(data=parentConfig.config[category], status=status.HTTP_200_OK)
             else:
-                return Response(data="The category {} does not exist".format(category), status=status.HTTP_204_NO_CONTENT)
+                return Response(data="No entry with the id={}".format(list_id), status=status.HTTP_204_NO_CONTENT)
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            return Response(data="The category {} does not exist".format(category), status=status.HTTP_204_NO_CONTENT)
 
     def delete(self, request, *args, **kwargs):
         """
         Delete an entry from the category
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            category = request.query_params.get('category')
-            list_id = int(request.query_params.get('id'))
-            paterntConfig = AnalysisConfig.objects.get(project=project)
-            if paterntConfig.config[category]:
-                if list_id >= 0 and list_id < len(paterntConfig.config[category]):
-                    paterntConfig.config[category].pop(int(list_id))
-                    paterntConfig.save()
-                    return Response(data=paterntConfig.config[category], status=status.HTTP_200_OK)
-                else:
-                    return Response(data="No entry with the id={}".format(list_id), status=status.HTTP_204_NO_CONTENT)
+        category = request.query_params.get('category')
+        list_id = int(request.query_params.get('id'))
+        parentConfig = AnalysisConfig.objects.get(project=project)
+        if parentConfig.config[category]:
+            if list_id >= 0 and list_id < len(parentConfig.config[category]):
+                parentConfig.config[category].pop(int(list_id))
+                parentConfig.save()
+                return Response(data=parentConfig.config[category], status=status.HTTP_200_OK)
             else:
-                return Response(data="The category {} does not exist".format(category), status=status.HTTP_204_NO_CONTENT)
+                return Response(data="No entry with the id={}".format(list_id), status=status.HTTP_204_NO_CONTENT)
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
-
+            return Response(data="The category {} does not exist".format(category), status=status.HTTP_204_NO_CONTENT)
 
 class Categories(APIView):
-    permission_classes = (IsAuthenticated,)
+    
     parser_classes = [FormParser]
 
     def get(self, request, *args, **kwargs):
@@ -98,32 +87,27 @@ class Categories(APIView):
         Return the existing categories in the solver config
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            config = AnalysisConfig.objects.get(project=project).config.keys()
-            return Response(data=config, status=status.HTTP_200_OK)
-        else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+        config = AnalysisConfig.objects.get(project=project).config.keys()
+        return Response(data=config, status=status.HTTP_200_OK)
+
 
     def delete(self, request, *args, **kwargs):
         """
         DELETE the existing categories in the solver config
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            category = request.query_params.get('category')
-            paterntConfig = AnalysisConfig.objects.get(project=project)
-            if category in paterntConfig.config:
-                del paterntConfig.config[category]
-                paterntConfig.save()
-                return Response(data=paterntConfig.config.keys(), status=status.HTTP_410_GONE)
-            else:
-                return Response(data="The category {} does not exist!".format(category), status=status.HTTP_404_NOT_FOUND)
+        category = request.query_params.get('category')
+        parentConfig = AnalysisConfig.objects.get(project=project)
+        if category in parentConfig.config:
+            del parentConfig.config[category]
+            parentConfig.save()
+            return Response(data=parentConfig.config.keys(), status=status.HTTP_410_GONE)
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            return Response(data="The category {} does not exist!".format(category), status=status.HTTP_404_NOT_FOUND)
 
 
 class getConfiguration(APIView):
-    permission_classes = (IsAuthenticated,)
+    
     parser_classes = [FormParser]
 
     def get(self, request, *args, **kwargs):
@@ -131,15 +115,13 @@ class getConfiguration(APIView):
         Get the solver config to be submitted to the analysis
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            config = AnalysisConfig.objects.filter(project=project).values()[0]
-            config.pop('visualizationMesh', None)
-            return Response(data=config, status=status.HTTP_200_OK)
-        else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+        config = AnalysisConfig.objects.filter(project=project).values()[0]
+        config.pop('visualizationMesh', None)
+        return Response(data=config, status=status.HTTP_200_OK)
+
 
 class solvers(APIView):
-    permission_classes = (IsAuthenticated,)
+    
     parser_classes = [FormParser]
 
     def get(self, request, *args, **kwargs):
@@ -147,32 +129,28 @@ class solvers(APIView):
         Runs the related solver defined in url parameters
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            # set progress to initial
-            progress = get_object_or_404(SolverProgress, project=project)
-            progress.progress = {'status':'RECEIVED', 'percent':'0'}
-            progress.save()
+        # set progress to initial
+        progress = get_object_or_404(SolverProgress, project=project)
+        progress.progress = {'status':'RECEIVED', 'percent':'0'}
+        progress.save()
 
-            # initiate related solver
-            solver = request.query_params.get('solver')
-            Cookie = request.headers['Cookie'].split(";")[0]
-            Authorization = 'bmFzc2VyOm5lcm8xMjM0' # this needs to be replaced with tocken authentication
-            client = docker.from_env()   
-            solverPath = os.path.abspath('./solvers')
-            client.containers.run(
-                "quay.io/fenicsproject/stable:current",
-                volumes={ solverPath :{'bind': '/home/fenics/shared', 'mode': 'rw'}},
-                working_dir="/home/fenics/shared",
-                command=["`python3 {}.py {} {} {}`".format(solver, project.id, Authorization, Cookie)], # runs solver.py with three arguments to be passed in to python file
-                auto_remove=False,
-                detach=True)
-            return Response(data="submitted to analysis", status=status.HTTP_200_OK)
-        else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+        # initiate related solver
+        solver = request.query_params.get('solver')
+
+        client = docker.from_env()   
+        solverPath = os.path.abspath('./solvers')
+        client.containers.run(
+            "quay.io/fenicsproject/stable:current",
+            volumes={ solverPath :{'bind': '/home/fenics/shared', 'mode': 'rw'}},
+            working_dir="/home/fenics/shared",
+            command=["`python3 {}.py {}`".format(solver, project.id)], # runs solver.py with three arguments to be passed in to python file
+            auto_remove=False,
+            detach=True)
+        return Response(data="submitted to analysis", status=status.HTTP_200_OK)
 
 
 class resutls(APIView):
-    permission_classes = (IsAuthenticated,)
+    
     parser_classes = [JSONParser]
 
     def get(self, request, *args, **kwargs):
@@ -180,38 +158,30 @@ class resutls(APIView):
         Get the results saved in the database
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            if (SolverResults.objects.filter(project=project).exists()):
-                resutls = SolverResults.objects.filter(project=project).values()[0]
-            else:
-                resutls = "not found"
-            return Response(data=resutls, status=status.HTTP_200_OK)
+        if (SolverResults.objects.filter(project=project).exists()):
+            resutls = SolverResults.objects.filter(project=project).values()[0]
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            resutls = "not found"
+        return Response(data=resutls, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         """
         save results from server to database
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-
-        if (project.user == request.user):
-            data = request.data
-            if SolverResults.objects.filter(project=project).exists():
-                solverResults = get_object_or_404(SolverResults, project=project)
-                solverResults.result = data
-                solverResults.save()
-            else:
-                SolverResults.objects.create(project=project, result=data)
-            
-            return Response(data="results updated", status=status.HTTP_201_CREATED)
+        data = request.data
+        if SolverResults.objects.filter(project=project).exists():
+            solverResults = get_object_or_404(SolverResults, project=project)
+            solverResults.result = data
+            solverResults.save()
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            SolverResults.objects.create(project=project, result=data)
+        
+        return Response(data="results updated", status=status.HTTP_201_CREATED)
+
 
 
 class solverProgress(APIView):
-    
-    permission_classes = (IsAuthenticated,)
     parser_classes = [JSONParser]
 
     def get(self, request, *args, **kwargs):
@@ -219,30 +189,23 @@ class solverProgress(APIView):
         Get the progress
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            if (SolverProgress.objects.filter(project=project).exists()):
-                progress = get_object_or_404(SolverProgress, project=project)
-            else:
-                progress = "no solver has been started yet"
-            return Response(data=progress.progress, status=status.HTTP_200_OK)
+        if (SolverProgress.objects.filter(project=project).exists()):
+            progress = get_object_or_404(SolverProgress, project=project)
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            progress = "no solver has been started yet"
+        return Response(data=progress.progress, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         """
         Update the progress from solver
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-
-        if (project.user == request.user):
-            data = request.data
-            if SolverProgress.objects.filter(project=project).exists():
-                progress = get_object_or_404(SolverProgress, project=project)
-                progress.progress = data
-                progress.save()
-            else:
-                SolverProgress.objects.create(project=project, progress=data)
-            
-            return Response(data=get_object_or_404(SolverProgress, project=project).progress, status=status.HTTP_201_CREATED)
+        data = request.data
+        if SolverProgress.objects.filter(project=project).exists():
+            progress = get_object_or_404(SolverProgress, project=project)
+            progress.progress = data
+            progress.save()
         else:
-            return Response(data="No permission to access this project", status=status.HTTP_401_UNAUTHORIZED)
+            SolverProgress.objects.create(project=project, progress=data)
+        
+        return Response(data=get_object_or_404(SolverProgress, project=project).progress, status=status.HTTP_201_CREATED)
--- FEniCSUI/MeshGenerator/views.py
+++ FEniCSUI/MeshGenerator/views.py
@@ -1,8 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import exceptions
-from rest_framework.permissions import IsAuthenticated
-from .OnShapeImport import OnShape
-from .models import visualizationMesh#, projects
+from .models import visualizationMesh, projects
 from .mesher import gmsh, mshReader
 from .serializer import bufferedGeometrySerializer
 from django.shortcuts import get_object_or_404
@@ -16,50 +13,7 @@ import shutil
 
 
 
-class geometryImport(APIView):
-    permission_classes = (IsAuthenticated,)
-    parser_classes = [FormParser]
-
-    def post(self, request, *args, **kwargs):
-        project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            data = request.data
-            if data["did"] and data["wid"] and data["eid"]:
-                geometry = OnShape(
-                    data["did"], data["wid"], data["eid"]
-                )
-                geometry.getStepFile(project.id)
-                if visualizationMesh.objects.filter(project=project).exists():
-                    record = get_object_or_404(
-                        visualizationMesh, project=project.id)
-                    record.mesh = geometry.getVisualizationMesh()
-                    record.faces = geometry.getFaces()
-                    record.edges = gmsh(project.id).visualizationEdgeMesh()
-                    record.points = gmsh(project.id).visualizationPoints()
-                    record.boundingBox = geometry.getBoundingBox()
-                    record.save()
-
-                else:
-                    visualizationMesh.objects.create(
-                        project=project,
-                        mesh=geometry.getVisualizationMesh(),
-                        edges=gmsh(project.id).visualizationEdgeMesh(),
-                        points=gmsh(project.id).visualizationPoints(),
-                        faces=geometry.getFaces(),
-                        boundingBox=geometry.getBoundingBox(),
-                    )
-                queryset = get_object_or_404(
-                    visualizationMesh, project=project)
-                serializer = bufferedGeometrySerializer(queryset)
-                return Response(serializer.data)
-            else:
-                return Response(data="Please ensure the OnShape document parameters are correct", status=400)
-        else:
-            return Response(data="No permission to access project", status=401)
-
-
 class mesher(APIView):
-    permission_classes = (IsAuthenticated,)
     parser_classes = [FormParser]
 
     def get(self, request, *args, **kwargs):
@@ -67,39 +21,32 @@ class mesher(APIView):
         Returns the mesh data from the database, if exists.
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            config = get_object_or_404(AnalysisConfig, project=project)
-            if config.mesh:
-                return Response(data=config.mesh, status=200)
-            else:
-                return Response(data="No mesh found. Please make sure you have meshed the geometry", status=204)
+        config = get_object_or_404(AnalysisConfig, project=project)
+        if config.mesh:
+            return Response(data=config.mesh, status=200)
 
     def post(self, request, *args, **kwargs):
         """
         Create and save computational mesh. Returns visualization mesh to the front end
         """
         project = get_object_or_404(projects, id=kwargs['project_id'])
-        if (project.user == request.user):
-            meshSize = float(request.data['meshSize'])
-            mesh = gmsh(project.id)
-            mesh.generateMesh(meshSize)
-            meshData = mshReader(project.id)
+        meshSize = float(request.data['meshSize'])
+        mesh = gmsh(project.id)
+        mesh.generateMesh(meshSize)
+        meshData = mshReader(project.id)
 
-            # save mesh to database
-            config = get_object_or_404(AnalysisConfig, project=project)
-            config.mesh = json.loads(meshData.readFile())
-            config.visualizationMesh = meshData.bufferedMesh
-            config.save()
+        # save mesh to database
+        config = get_object_or_404(AnalysisConfig, project=project)
+        config.mesh = json.loads(meshData.readFile())
+        config.visualizationMesh = meshData.bufferedMesh
+        config.save()
 
-            
-            # return buffered geometry for visualization in ThreeJS
-            return Response(meshData.bufferedMesh)
-        else:
-            return Response(data="No permission to access project", status=401)
+        
+        # return buffered geometry for visualization in ThreeJS
+        return Response(meshData.bufferedMesh)
 
 
 class uploadStep(APIView):
-    permission_classes = (IsAuthenticated,)
     parser_classes = [FileUploadParser]
 
     def post(self, request, filename, format=None, *args, **kwargs):
@@ -108,33 +55,29 @@ class uploadStep(APIView):
         """
         project_id = kwargs['project_id']
         project = get_object_or_404(projects, id=project_id)
-        if (project.user == request.user):
-            stepFile = request.data['file']
-            folderPath = os.path.abspath(
-                "../FEniCSUI/media/{}".format(project_id))
-            os.makedirs(folderPath, exist_ok=True)
-            with open('{}/stepFile.step'.format(folderPath), 'wb+') as destination:
-                for chunk in stepFile.chunks():
-                    destination.write(chunk)
+        stepFile = request.data['file']
+        folderPath = os.path.abspath(
+            "../FEniCSUI/media/{}".format(project_id))
+        os.makedirs(folderPath, exist_ok=True)
+        with open('{}/stepFile.step'.format(folderPath), 'wb+') as destination:
+            for chunk in stepFile.chunks():
+                destination.write(chunk)
 
-            meshSize = .2
-            mesh = gmsh(project.id)
-            mesh.generateMesh(meshSize)
-            meshData = mshReader(project.id)
+        meshSize = .2
+        mesh = gmsh(project.id)
+        mesh.generateMesh(meshSize)
+        meshData = mshReader(project.id)
 
-            # save mesh to database
-            config = get_object_or_404(AnalysisConfig, project=project)
-            config.mesh = json.loads(meshData.readFile())
-            config.visualizationMesh = meshData.bufferedMesh
-            config.save()
+        # save mesh to database
+        config = get_object_or_404(AnalysisConfig, project=project)
+        config.mesh = json.loads(meshData.readFile())
+        config.visualizationMesh = meshData.bufferedMesh
+        config.save()
 
-            # return buffered geometry for visualization in ThreeJS
-            return Response(meshData.bufferedMesh)
-        else:
-            return Response(data="No permission to access project", status=401)
+        # return buffered geometry for visualization in ThreeJS
+        return Response(meshData.bufferedMesh)
 
 class getVisualizationMesh(APIView):
-    permission_classes = (IsAuthenticated,)
     parser_classes = [FileUploadParser]
 
     def get(self, request, *args, **kwargs):
@@ -143,11 +86,9 @@ class getVisualizationMesh(APIView):
         """
         project_id = kwargs['project_id']
         project = get_object_or_404(projects, id=project_id)
-        if (project.user == request.user):
-            config = get_object_or_404(AnalysisConfig, project=project)
-            if (config.visualizationMesh): 
-                return Response(config.visualizationMesh)
-            else:
-                return Response(data="No geometry has imported", status=404)
+        config = get_object_or_404(AnalysisConfig, project=project)
+        if (config.visualizationMesh): 
+            return Response(config.visualizationMesh)
         else:
-            return Response(data="No permission to access project", status=401)
+            return Response(data="No geometry has imported", status=404)
+
